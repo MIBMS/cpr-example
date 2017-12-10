63bceeda66acd8f3fdfc4550f599cafb633b44f6
Ilham Salleh <ilhamsalleh91@gmail.com>
2017-12-10 10:41:59 +0800

    mingw64

--- example.cpp
+++ example.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <string>
 
 #include <cpr/cpr.h>
 #include <json.hpp>
 
 
+#include<conio.h>
+
+std::string getpass() {
+	std::string a = "";
+	char c;
+	for (int i = 0; i<1000; i++)
+	{
+		c = getch();
+		if (c == '\r')
+			break;
+		std::cout << "*";
+		a += c;
+	}
+	return a;
+}
+
+
 int main(int argc, char** argv) {
-    auto response = cpr::Get(cpr::Url{"https://httpbin.org/get"});
+	std::string password = getpass();
+    auto response = cpr::Get(cpr::Url{"https://ocp-console-risk.np.ocp.standardchartered.com:8443/oauth/authorize?client_id=openshift-challenging-client&response_type=token"},
+		cpr::Header{ {"X-CSRF-Token", "1"} },
+		cpr::Authentication{"1576835", password});
     auto json = nlohmann::json::parse(response.text);
     std::cout << json.dump(4) << std::endl;
+	std::cout << response.text << std::endl;
 }
